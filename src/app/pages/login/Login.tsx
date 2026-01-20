79e197cf679af3a92148f4f7277a354664efdbaf
Luan Rubiane <luanrubiane@hotmail.com>
2026-01-20 16:07:07 -0300

    feat: creat generic reusable component input

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,31 +1,44 @@
-import { useState } from "react";
+import { useCallback, useRef, useState } from "react";
+import { InputLogin } from "./components/inputLogin";
 
 export const Login = () => {
+    const inputPasswordRef= useRef<HTMLInputElement>(null);
+
     const[password, setPassword] = useState('');
     const[email, setEmail] = useState('');
 
-    const handleEntrar = () => {
+    const handleEntrar = useCallback(() => {
         console.log(email)
         console.log(password)
-    };  
+    }, [email,password]);  
 
     return(
           <div>
             <form>
-                    <label>
-                        <span>Email</span>
-                        <input 
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}/>
-                    </label>
+                    <InputLogin
+                    label="Email"
+                    value={email}
+                    onChange= {newValue => setEmail(newValue)}
+                    onPressEnter={() => inputPasswordRef.current?.focus()}
+                    />
 
-                    <label>
+                    <InputLogin
+                    label="Senha"
+                    type="password"
+                    value={password}
+                    onChange= {newValue => setPassword(newValue)}
+                    ref= {inputPasswordRef}
+                    />
+
+                    {/* <label>
                         <span>Senha</span>
                         <input 
                         type="password" 
                         value={password} 
-                        onChange={e => setPassword(e.target.value)}/>
-                    </label>
+                        ref= {inputPasswordRef}
+                        onChange={e => setPassword(e.target.value)}
+                        />
+                    </label> */}
                     <button type="button" onClick={handleEntrar}>Entrar</button>
             </form>   
         </div>
