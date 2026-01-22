085a8fc51ddba9f3bdb2bb3d768d247b725b3736
Luan Rubiane <luanrubiane@hotmail.com>
2026-01-22 10:13:09 -0300

    feat: add feature to use hook useRef

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,19 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 
 export const Dashboard = () => {
+    const counterRef = useRef({ counter: 0 })
+    console.log(counterRef)
+
     return (
     <div>  
         <p>Dashboard</p>
+
+        <p>Contador: {counterRef.current.counter} </p>
+
+        <button onClick={() => counterRef.current.counter++}>Somar</button>
+
         <Link to="/entrar">Login</Link>
     </div>
     );
